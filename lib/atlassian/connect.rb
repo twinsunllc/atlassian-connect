beaf6c23e2d62d1a6fccbbce3e59b0efe61a41d9
NotDaveLane <dclane@gmail.com>
2021-02-25 16:51:32 -0600

    Starting to add configuration options to the engine.

--- lib/atlassian/connect.rb
+++ lib/atlassian/connect.rb
@@ -4,5 +4,27 @@ require "atlassian/connect/version"
 
 module Atlassian
   module Connect
+    mattr_accessor :api_version
+    self.api_version = 1
+
+    mattr_accessor :enable_licensing
+
+    mattr_accessor :key
+
+    mattr_accessor :modules
+
+    mattr_accessor :name
+
+    mattr_accessor :post_install_url
+
+    mattr_accessor :post_update_url
+
+    mattr_accessor :scopes
+    self.scopes = [
+        'act_as_user', 'read', 'write'
+    ]
+
+    mattr_accessor :vendor_name
+    mattr_accessor :vendor_url
   end
 end
